dd6be096d46df00d0572300ae5702a86fe962f1b
purplesunk <purplesunk@github.com>
2022-10-21 20:54:00 -0300

    mover funciones

--- funciones.cpp
+++ funciones.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
+#include "funciones.h"
 
-int main() {
+void linea() {
+    std::cout << "---------------------------------------------------\n";
+}
 
+void cargarVectorRandom(int vec[], int tam, int tope) {
+    for (int i=0; i<tam; i++) {
+        vec[i]=(rand()%tope)+1;
+    }
+}
 
-    return 0;
+void mostrarVec(int vec[], int tam) {
+    for (int i=0; i<tam; i++) {
+        std::cout << vec[i] << "\t";
+    }
+    std::cout << "\n";
+}
+
+int maxVector(int vec[], int tam) {
+    int maximo = vec[0];
+
+    for (int i=1; i<tam; i++) {
+        if (vec[i]>maximo) {
+            maximo=vec[i];
+        }
+    }
+
+    return maximo;
+}
+
+int posMaxVector(int vec[], int tam) {
+    int maximo = vec[0];
+    int pos = 1;
+
+    for (int i=1; i<tam; i++) {
+        if (vec[i]>maximo) {
+            maximo=vec[i];
+            pos=i+1;
+        }
+    }
+
+    return pos;
+}
+
+bool hayCeros(int vec[], int tam){
+    bool hayCero = false;
+    for (int i=0;i<tam;i++) {
+        if(vec[i]==0) {
+            hayCero = true;
+        }
+    }
+    return hayCero;
 }
--- funciones.h
+++ funciones.h
@@ -1,6 +1,11 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-
+void linea();
+void cargarVectorRandom(int vec[], int tam, int tope);
+void mostrarVec(int vec[], int tam);
+int maxVector(int vec[], int tam);
+int posMaxVector(int vec[], int tam);
+bool hayCeros(int vec[], int tam);
 
 #endif // FUNCIONES_H_INCLUDED
