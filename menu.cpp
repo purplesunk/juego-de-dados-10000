cef34063190f7b9ee859f93dc7f73fee32541e91
purplesunk <purplesunk@github.com>
2022-10-22 16:17:16 -0300

    opciones mas centradas

--- menu.cpp
+++ menu.cpp
@@ -47,10 +47,10 @@ void menu() {
         setDefault();
         rlutil::hidecursor();
 
-        mostrarOpcion(" MODO UN JUGADOR        ", centrox, centroy, y == 0);
-        mostrarOpcion(" MODO DOS JUGADORES     ", centrox, centroy+1, y == 1);
-        mostrarOpcion(" MEJORES PUNTAJES       ", centrox, centroy+2, y == 2);
-        mostrarOpcion(" SALIR                  ", centrox, centroy+3, y == 3);
+        mostrarOpcion("   MODO UN JUGADOR      ", centrox, centroy, y == 0);
+        mostrarOpcion("   MODO DOS JUGADORES   ", centrox, centroy+1, y == 1);
+        mostrarOpcion("   MEJORES PUNTAJES     ", centrox, centroy+2, y == 2);
+        mostrarOpcion("   SALIR                ", centrox, centroy+3, y == 3);
 
 
         switch (rlutil::getkey()) {
